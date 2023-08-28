13a16464f6eac6311266f697619cf96344fb6150
Umesh Sharma <umesh.kdev221@gmail.com>
2023-08-28 21:11:58 +0530

    Episode 04

---
+++ Episode_04/Header.js
@@ -0,0 +1,16 @@
+const Header = () => (
+    <div className="header">
+      <div className="logo">
+      <img src="https://img.freepik.com/premium-vector/mascot-character-design-standing-hotdog-badge_697324-13003.jpg?t=st=1692847728~exp=1692848328~hmac=d88fd86411a811f27dd8446efd53ecb3f5ff1b069fca0522911e9d1f473deac0" alt="" />
+      </div>
+      <div className="search">
+        <input type="text" />
+        <button> Search </button>
+      </div>
+      <div className="icons">
+        <img src="https://img.icons8.com/?size=1x&id=42384&format=png" alt="" />
+      </div>
+    </div>
+)
+
+export default Header;
